5d2947cc56a10f91dc944ed883374cc6d642efc9
Fyrgeit <mans@peronilla.se>
2024-01-12 11:28:32 +0100

    makeit looka lota better

--- app.js
+++ app.js
@@ -18,52 +18,70 @@ const templates = {
     "Sl": "m0 10 l10 0 l10 -10",
     "Sl+": "m0 10 l20 0 m-5 -5 l5 -5",
     "Sl-": "m0 10 l10 0 l10 -10 m0 10 l-3 0",
-    
-    "Se": "m0 10 l10 0",
-    "eS": "m10 10 l10 0",
 };
 
-const tiles = [
-    { x: 0, y: 1, template: "eS", downSignal: "red", upSignal: "red" },
-    { x: 0, y: 2, template: "eS", downSignal: "red", upSignal: "red" },
-    
+const rails = [
     { x: 1, y: 1, template: "S" },
     { x: 1, y: 2, template: "S" },
-    
+
     { x: 2, y: 1, template: "S" },
     { x: 2, y: 2, template: "S" },
-    
-    { x: 3, y: 1, template: "Sr+" },
-    { x: 3, y: 2, template: "Sl+" },
-    
-    { x: 4, y: 1, template: "lS+" },
-    { x: 4, y: 2, template: "rS+" },
-    
-    { x: 5, y: 1, template: "Sl+" },
+
+    { x: 3, y: 1, template: "Sr+", switchIndex: "A" },
+    { x: 3, y: 2, template: "Sl+", switchIndex: "B" },
+
+    { x: 4, y: 1, template: "lS+", switchIndex: "C" },
+    { x: 4, y: 2, template: "rS+", switchIndex: "D" },
+
+    { x: 5, y: 1, template: "Sl+", switchIndex: "E" },
     { x: 5, y: 2, template: "S" },
-    
-    { x: 6, y: 0, template: "lS", downSignal: "red" },
-    { x: 6, y: 1, template: "S", downSignal: "red" },
-    { x: 6, y: 2, template: "S", downSignal: "red" },
-    
+
+    { x: 6, y: 0, template: "lS" },
+    { x: 6, y: 1, template: "S" },
+    { x: 6, y: 2, template: "S" },
+
     { x: 7, y: 0, template: "S" },
     { x: 7, y: 1, template: "S" },
     { x: 7, y: 2, template: "S" },
-    
-    { x: 8, y: 0, template: "S", upSignal: "red" },
-    { x: 8, y: 1, template: "S", upSignal: "red" },
-    { x: 8, y: 2, template: "Sl", upSignal: "red" },
-    
-    { x: 9, y: 0, template: "Sr+" },
-    { x: 9, y: 1, template: "lS+" },
-    
-    { x: 10, y: 0, template: "Se", upSignal: "red", downSignal: "red" },
-    { x: 10, y: 1, template: "rS+" },
-    
-    { x: 11, y: 1, template: "S", downSignal: "red" },
+
+    { x: 8, y: 0, template: "Sr" },
+    { x: 8, y: 1, template: "Sr+", switchIndex: "F" },
+    { x: 8, y: 2, template: "S" },
+
+    { x: 9, y: 1, template: "rS+", switchIndex: "G" },
+    { x: 9, y: 2, template: "rS+", switchIndex: "H" },
+
+    { x: 10, y: 1, template: "S" },
+    { x: 10, y: 2, template: "Sr" },
+
+    { x: 11, y: 1, template: "S" },
+    { x: 11, y: 3, template: "rS" },
+
     { x: 12, y: 1, template: "S" },
-    { x: 13, y: 1, template: "Se", upSignal: "red", downSignal: "red" },
+    { x: 12, y: 3, template: "S" },
 ];
+
+const signals = [
+    { x: 0, y: 1, type: "down", state: "red" },
+    { x: 0, y: 2, type: "down", state: "red" },
+
+    { x: 2, y: 1, type: "up", state: "red" },
+    { x: 2, y: 2, type: "up", state: "red" },
+
+    { x: 6, y: 0, type: "down", state: "red" },
+    { x: 6, y: 1, type: "down", state: "red" },
+    { x: 6, y: 2, type: "down", state: "red" },
+
+    { x: 8, y: 0, type: "up", state: "red" },
+    { x: 8, y: 1, type: "up", state: "red" },
+    { x: 8, y: 2, type: "up", state: "red" },
+
+    { x: 11, y: 1, type: "down", state: "red" },
+    { x: 11, y: 3, type: "down", state: "red" },
+
+    { x: 13, y: 1, type: "up", state: "red" },
+    { x: 13, y: 3, type: "up", state: "red" },
+]
 
 const canvas = document.getElementById("canvas");
 
@@ -74,94 +92,110 @@ refresh();
 function refresh() {
     canvas.innerHTML = "";
     
-    tiles.forEach((el) => {
+    const maxWidth = Math.max(...rails.concat(signals).map(o => o.x)) * gridSize + gridSize;
+    const maxHeight = Math.max(...rails.concat(signals).map(o => o.y)) * gridSize + gridSize;
+
+    let xArr = [];
+    let yArr = [];
+
+    for (let x = gridSize; x < maxWidth; x += gridSize) {
+        xArr.push(x);
+    }
+
+    for (let y = gridSize; y < maxHeight; y += gridSize) {
+        yArr.push(y);
+    }
+
+    const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    canvas.append(pathEl);
+    pathEl.classList.add("grid-line");
+    pathEl.setAttribute("d", xArr.map(x => `M${x} 0 L${x} ${maxHeight}`).concat(yArr.map(y => `M0 ${y} L${maxWidth} ${y}`)).join(" "));
+
+    rails.forEach(railObj => {
         const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         canvas.append(pathEl);
         pathEl.classList.add("line");
-        pathEl.setAttribute("d", `M${el.x * gridSize} ${el.y * gridSize} ${templates[el.template]}`);
+        pathEl.setAttribute("d", `M${railObj.x * gridSize} ${railObj.y * gridSize} ${templates[railObj.template]}`);
 
-        if (el.upSignal) {
-            const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            canvas.append(pathEl);
-            pathEl.classList.add("signal");
-            pathEl.classList.add(el.upSignal);
-            pathEl.setAttribute("d", `M${el.x * gridSize} ${el.y * gridSize} m6 6 l4 4 l-4 4`);
-            pathEl.setAttribute("pos", el.x + ", " + el.y);
-            pathEl.addEventListener("click", event => {
-                let pos = event.target.getAttribute("pos");
-                
-                let e = tiles.find(el => el.x == pos.split(", ")[0] && el.y == pos.split(", ")[1]);
-                
-                if (e.upSignal === "green") {
-                    e.upSignal = "red";
-                } else {
-                    e.upSignal = "green";
-                }
-                
-                refresh();
-            });
-        }
-        
-        if (el.downSignal) {
-            const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            canvas.append(pathEl);
-            pathEl.classList.add("signal");
-            pathEl.classList.add(el.downSignal);
-            pathEl.setAttribute("d", `M${el.x * gridSize} ${el.y * gridSize} m14 6 l-4 4 l4 4`);
-            pathEl.setAttribute("pos", el.x + ", " + el.y);
-            pathEl.addEventListener("click", event => {
-                let pos = event.target.getAttribute("pos");
-                
-                let e = tiles.find(el => el.x == pos.split(", ")[0] && el.y == pos.split(", ")[1]);
-                
-                if (e.downSignal === "green") {
-                    e.downSignal = "red";
-                } else {
-                    e.downSignal = "green";
-                }
-                
-                refresh();
-            });
-        }
-        
-        if (el.template.includes("+") || el.template.includes("-")) {
+        if (railObj.template.includes("+") || railObj.template.includes("-")) {
             const switchEl = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
             canvas.append(switchEl);
             switchEl.classList.add("switch-blob");
-            switchEl.setAttribute("cx", 10 + el.x * gridSize);
-            switchEl.setAttribute("cy", 10 + el.y * gridSize);
-            switchEl.setAttribute("r", "3");
-            switchEl.setAttribute("pos", el.x + ", " + el.y);
-            switchEl.addEventListener("click", event => {
-                let pos = event.target.getAttribute("pos");
-                
-                let e = tiles.find(el => el.x == pos.split(", ")[0] && el.y == pos.split(", ")[1]);
-
-                if (e.template.includes("+")) {
-                    e.template = e.template.replace("+", "-")
+            switchEl.setAttribute("cx", 10 + railObj.x * gridSize);
+            switchEl.setAttribute("cy", 10 + railObj.y * gridSize);
+            switchEl.setAttribute("r", "2");
+            switchEl.addEventListener("click", () => {
+                if (railObj.template.includes("+")) {
+                    railObj.template = railObj.template.replace("+", "-")
                 } else {
-                    e.template = e.template.replace("-", "+")
+                    railObj.template = railObj.template.replace("-", "+")
                 }
 
                 refresh();
-            })
-        }
+            });
 
-        if (el.x == playerPos.x && el.y == playerPos.y) {
-            const playerEl = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            canvas.append(playerEl);
-            playerEl.setAttribute("cx", 10 + el.x * gridSize);
-            playerEl.setAttribute("cy", 10 + el.y * gridSize);
-            playerEl.setAttribute("r", "4");
-            playerEl.setAttribute("fill", "red");
-        }
+            const textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            canvas.append(textEl);
+            textEl.classList.add("text");
+            textEl.innerHTML = railObj.switchIndex;
 
+            const typeOffset = {x: 10, y: 7};
+
+            typeOffset.x -= 1.2 * railObj.switchIndex.toString().length;
+
+            textEl.setAttribute("x", railObj.x * gridSize + typeOffset.x);
+            textEl.setAttribute("y", railObj.y * gridSize + typeOffset.y);
+        }
     });
 
+    signals.forEach((signalObj, index) => {
+        const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        canvas.append(pathEl);
+        pathEl.classList.add("signal");
+        pathEl.classList.add(signalObj.state);
+        
+        const typePath = {
+            "up": "m2 6 l4 4 l-4 4",
+            "down": "m18 6 l-4 4 l4 4",
+        }[signalObj.type];
+        
+        pathEl.setAttribute("d", `M${signalObj.x * gridSize} ${signalObj.y * gridSize} ${typePath}`);
+        
+        pathEl.addEventListener("click", () => {
+            if (signalObj.state === "green") {
+                signalObj.state = "red";
+            } else {
+                signalObj.state = "green";
+            }
+            
+            refresh();
+        });
+        
+        const textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        canvas.append(textEl);
+        textEl.classList.add("text");
+        textEl.innerHTML = index;
+
+        const typeOffset = {
+            "up": {x: 4, y: 5},
+            "down": {x: 16, y: 5},
+        }[signalObj.type];
+
+        typeOffset.x -= 1.2 * index.toString().length;
+
+        textEl.setAttribute("x", signalObj.x * gridSize + typeOffset.x);
+        textEl.setAttribute("y", signalObj.y * gridSize + typeOffset.y);
+    });
+
+    const playerEl = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    canvas.append(playerEl);
+    playerEl.setAttribute("cx", 10 + playerPos.x * gridSize);
+    playerEl.setAttribute("cy", 10 + playerPos.y * gridSize);
+    playerEl.setAttribute("r", "4");
+    playerEl.setAttribute("fill", "red");
+
     canvas.setAttribute("viewBox",
-        "0 0 " +     
-        (tiles.sort((a, b) => b.x - a.x)[0].x * gridSize + gridSize + 8) + " " +
-        (tiles.sort((a, b) => b.y - a.y)[0].y * gridSize + gridSize + 8)
+        `0 0 ${maxWidth} ${maxHeight}`
     );
 }
 
@@ -170,10 +204,10 @@ function move(dir) {
         console.error("Invalid direction");
         return;
     }
-    
+
     let ogPlayerPos = { ...playerPos };
 
-    let fromTile = tiles.find(t => t.x === playerPos.x && t.y === playerPos.y);
+    let fromTile = rails.find(t => t.x === playerPos.x && t.y === playerPos.y);
 
     switch (dir) {
         case "up":
@@ -209,28 +243,30 @@ function move(dir) {
         default: break;
     }
 
-    let toTile = tiles.find(t => t.x === playerPos.x && t.y === playerPos.y);
+    let toSignal = signals.find(s => s.x === playerPos.x && s.y === playerPos.y);
 
-    switch (dir) {
-        case "up":
-            if (toTile.upSignal) {
-                if (toTile.upSignal === "red") {
-                    playerPos = { ...ogPlayerPos };
-                } else {
-                    toTile.upSignal = "red";
+    if (toSignal) {
+        switch (dir) {
+            case "up":
+                if (toSignal.type === "up") {
+                    if (toSignal.state === "red") {
+                        playerPos = { ...ogPlayerPos };
+                    } else {
+                        toSignal.state = "red";
+                    }
                 }
-            }
-            break;
-        case "down":
-            if (toTile.downSignal) {
-                if (toTile.downSignal === "red") {
-                    playerPos = { ...ogPlayerPos };
-                } else {
-                    toTile.downSignal = "red";
+                break;
+            case "down":
+                if (toSignal.type === "down") {
+                    if (toSignal.state === "red") {
+                        playerPos = { ...ogPlayerPos };
+                    } else {
+                        toSignal.state = "red";
+                    }
                 }
-            }
-            break;
-        default: break;
+                break;
+            default: break;
+        }
     }
 
     refresh();
