b81b84d46632a36dbe93c2a3aff75f432fc95e9e
Fyrgeit <mans@peronilla.se>
2024-01-29 09:24:27 +0100

    goodnight

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import data from "./data.json" assert {type: "json"};
 
-data.switches = data.rails.filter(rail => rail.switchIndex).map(r => r.switchIndex);
+data.switches = data.rails.filter(rail => rail.switchIndex);
 
 const templates = {
     "S": "m0 10 l20 0",
@@ -29,37 +29,43 @@ let playerPos = { x: 1, y: 1 };
 
 let routeInfoRails = [];
 
+document.getElementById("bLeft").onclick = () => { movePlayer("down"); };
+document.getElementById("bRight").onclick = () => { movePlayer("up"); };
 
-const refresh = () => {
+refreshDisplay();
+refreshTable();
+
+function refreshDisplay() {
     canvas.innerHTML = "";
-    
+
     //Draw grid
     const maxWidth = Math.max(...data.rails.concat(data.signals).map(o => o.x)) * gridSize + gridSize;
     const maxHeight = Math.max(...data.rails.concat(data.signals).map(o => o.y)) * gridSize + gridSize;
-    
+
     let xArr = [];
     let yArr = [];
-    
+
     for (let x = gridSize; x < maxWidth; x += gridSize) {
         xArr.push(x);
     }
-    
+
     for (let y = gridSize; y < maxHeight; y += gridSize) {
         yArr.push(y);
     }
-    
+
     const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     canvas.append(pathEl);
     pathEl.classList.add("grid-line");
     pathEl.setAttribute("d", xArr.map(x => `M${x} 0 L${x} ${maxHeight}`).concat(yArr.map(y => `M0 ${y} L${maxWidth} ${y}`)).join(" "));
-    
+
     //Draw rails
     data.rails.forEach(railObj => {
         const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         canvas.append(pathEl);
         pathEl.classList.add("line");
         pathEl.setAttribute("d", `M${railObj.x * gridSize} ${railObj.y * gridSize} ${templates[railObj.template]}`);
-        
+
+        //Switches
         if (railObj.template.includes("+") || railObj.template.includes("-")) {
             const switchEl = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
             canvas.append(switchEl);
@@ -67,70 +73,87 @@ const refresh = () => {
             switchEl.setAttribute("cx", 10 + railObj.x * gridSize);
             switchEl.setAttribute("cy", 10 + railObj.y * gridSize);
             switchEl.setAttribute("r", "2");
-            switchEl.addEventListener("click", () => {
-                if (railObj.template.includes("+")) {
-                    railObj.template = railObj.template.replace("+", "-")
-                } else {
-                    railObj.template = railObj.template.replace("-", "+")
-                }
-                
-                refresh();
-            });
             
+            switchEl.addEventListener("click", () => {
+                if (!railObj.locked) {
+                    if (railObj.template.includes("+")) {
+                        railObj.template = railObj.template.replace("+", "-");
+                    } else {
+                        railObj.template = railObj.template.replace("-", "+");
+                    }
+                }
+
+                refreshDisplay();
+                refreshTable();
+            });
+
+            switchEl.addEventListener("contextmenu", (e) => {
+                e.preventDefault();
+
+                railObj.locked = !railObj.locked;
+
+                refreshDisplay();
+                refreshTable();
+            });
+
+            if (railObj.locked) {
+                switchEl.classList.add("locked");
+            }
+
             const textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
             canvas.append(textEl);
             textEl.classList.add("text");
             textEl.innerHTML = railObj.switchIndex;
-            
-            const typeOffset = {x: 10, y: 7};
-            
+
+            const typeOffset = { x: 10, y: 7 };
+
             typeOffset.x -= 1.2 * railObj.switchIndex.toString().length;
-            
+
             textEl.setAttribute("x", railObj.x * gridSize + typeOffset.x);
             textEl.setAttribute("y", railObj.y * gridSize + typeOffset.y);
         }
     });
-    
+
     //Draw signals
     data.signals.forEach((signalObj, index) => {
         const pathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         canvas.append(pathEl);
         pathEl.classList.add("signal");
         pathEl.classList.add(signalObj.state);
-        
+
         const typePath = {
             "up": "m2 6 l4 4 l-4 4",
             "down": "m18 6 l-4 4 l4 4",
         }[signalObj.type];
-        
+
         pathEl.setAttribute("d", `M${signalObj.x * gridSize} ${signalObj.y * gridSize} ${typePath}`);
-        
+
         pathEl.addEventListener("click", () => {
             if (signalObj.state === "green") {
                 signalObj.state = "red";
             } else {
                 signalObj.state = "green";
             }
-            
-            refresh();
+
+            refreshDisplay();
         });
-        
+
         const textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
         canvas.append(textEl);
         textEl.classList.add("text");
         textEl.innerHTML = index;
-        
+
         const typeOffset = {
-            "up": {x: 4, y: 5},
-            "down": {x: 16, y: 5},
+            "up": { x: 4, y: 5 },
+            "down": { x: 16, y: 5 },
         }[signalObj.type];
-        
+
         typeOffset.x -= 1.2 * index.toString().length;
-        
+
         textEl.setAttribute("x", signalObj.x * gridSize + typeOffset.x);
         textEl.setAttribute("y", signalObj.y * gridSize + typeOffset.y);
     });
-    
+
     //Draw player
     const playerEl = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
     canvas.append(playerEl);
@@ -139,8 +162,8 @@ const refresh = () => {
     playerEl.setAttribute("y", 7 + playerPos.y * gridSize);
     playerEl.setAttribute("width", "12");
     playerEl.setAttribute("height", "6");
-    
-    
+
+    //Display route
     routeInfoRails.forEach(railPos => {
         const rectEl = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         canvas.append(rectEl);
@@ -150,50 +173,78 @@ const refresh = () => {
         rectEl.setAttribute("width", gridSize);
         rectEl.setAttribute("height", gridSize);
     });
-    
+
     canvas.setAttribute("viewBox",
-    `0 0 ${maxWidth} ${maxHeight}`
+        `0 0 ${maxWidth} ${maxHeight}`
     );
 };
 
-document.getElementById("bLeft").onclick = () => { move("down"); };
-document.getElementById("bRight").onclick = () => { move("up"); };
+function refreshTable() {
+    const tableEl = document.querySelector("table");
 
-const tableEl = document.querySelector("table");
-
-tableEl.innerHTML = `
-    <tr>
-        <th>Signals</th>
-        <th>${data.switches.join("</th><th>")}</th>
-    </tr>
-`;
-
-for (let routeKey in data.routes) {
-    const route = data.routes[routeKey];
-    const rowEl = document.createElement("tr");
-    tableEl.append(rowEl);
-    
-    let newArr = [];
-    data.switches.forEach(s => {
-        newArr.push(route.switchStates[s]);
-    });
-
-    rowEl.innerHTML = `
-        <td>${routeKey}</td>
-        <td>${newArr.join("</td><td>")}</td>
+    tableEl.innerHTML = `
+        <tr>
+            <th>Route</th>
+            <th>${data.switches.map(s => s.switchIndex).join("</th><th>")}</th>
+            <th>OK</th>
+            <th>Put</th>
+        </tr>
+        <tr>
+            <th>Locked</th>
+            <th>${data.switches.map(s => s.locked ? "🔒" : "🆓").join("</th><th>")}</th>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th>Switch</th>
+            <th>${data.switches.map(s => s.template.slice(-1)).join("</th><th>")}</th>
+            <th></th>
+            <th></th>
+        </tr>
     `;
-};
 
-refresh();
+    for (let routeKey in data.routes) {
+        const route = data.routes[routeKey];
+        const rowEl = document.createElement("tr");
+        tableEl.append(rowEl);
 
-function move(dir) {
+        let columns = data.switches.map(s => ({
+            requestedSwitchState: route.switchStates[s.switchIndex],
+            currentSwitchState: s.template.slice(-1),
+            locked: s.locked,
+        }));
+
+        rowEl.innerHTML = `
+            <td>${routeKey}</td>
+            <td>${columns.map(c => c.requestedSwitchState).join("</td><td>")}</td>
+            <td>${requestedSwitchesCorrect(columns) ? "✅" : "⛔️"}</td>
+            <td><input type="checkbox" /></td>
+        `;
+    };
+}
+
+function requestedSwitchesCorrect(columns) {
+    return columns.every(c => {
+        if (c.requestedSwitchState == undefined) {
+            return true;
+        }
+
+        if (!c.locked) {
+            return false;
+        }
+
+        return c.requestedSwitchState == c.currentSwitchState;
+    });
+}
+
+function movePlayer(dir) {
     if (!["up", "down"].includes(dir)) {
         console.error("Invalid direction");
         return;
     }
-    
+
     let ogPlayerPos = { ...playerPos };
-    
+
     let fromTile = data.rails.find(t => t.x === playerPos.x && t.y === playerPos.y);
 
     switch (dir) {
@@ -256,44 +307,5 @@ function move(dir) {
         }
     }
 
-    refresh();
+    refreshDisplay();
 }
-
-document.getElementById("remove").onclick = () => {
-    routeInfoRails = [];
-    refresh();
-};
-
-document.getElementById("route-checker").addEventListener("submit", e => {
-    e.preventDefault();
-
-    let from = document.getElementById("from-signal").value;
-    let to = document.getElementById("to-signal").value;
-
-    const outputEl = document.getElementById("output");
-
-    if (
-        from === "" ||
-        from < 0 ||
-        from >= data.signals.length ||
-        to === "" ||
-        to < 0 ||
-        to >= data.signals.length
-    ) {
-        outputEl.innerText = "Invalid signals";
-        return;
-    }
-    
-    let routeInfo = data.routes[from + " " + to];
-    
-    if (!routeInfo) {
-        outputEl.innerText = "No such route exists";
-        return;
-    }
-
-    routeInfoRails = routeInfo.usedRails;
-    
-    outputEl.innerText = JSON.stringify(routeInfo);
-
-    refresh();
-});
